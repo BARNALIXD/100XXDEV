e9e3c637fe48673e423345bc9726e2e7fc07dc26
user.name.like : "barnalixd <user.email.like : dasbarnali690@gcekbpatna.ac.in>
2025-11-30 09:51:00 +0530

    prints directly

--- index.js
+++ index.js
@@ -1,11 +1,7 @@
 let name = "barnali";
 const age = 25;
+var isStudent = true;
 
-greet();
-
-function greet() {
-  console.log(`Hello, my name is ${name} and I am ${age} years old.`);
-}
-
-
-
+console.log( name);
+console.log( age);
+console.log( isStudent);  

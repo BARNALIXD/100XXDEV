44d5ae79b3d172b3fd7442fe75677ed05663aa33
user.name.like : "barnalixd <user.email.like : dasbarnali690@gcekbpatna.ac.in>
2025-11-30 10:13:03 +0530

    Array logging in js

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-var isStudent = true;
-console.log(isStudent);
-var isStudent = false;
-console.log(isStudent); 
+let users = ['harkirat', 'jasleen', 'manpreet'];
+
+console.log(users[0]);
+console.log(users[1]);
+console.log(users[2]);
